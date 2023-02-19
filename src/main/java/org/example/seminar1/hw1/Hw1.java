395dc6edd12fc5e36314e86e4251524c91a4567b
MaSlaff <maslaff@yandex.ru>
2023-02-19 21:19:50 +0300

    HW1_Solved

--- src/main/java/org/example/seminar1/hw1/Hw1.java
+++ src/main/java/org/example/seminar1/hw1/Hw1.java
@@ -1,4 +1,14 @@
 package org.example.seminar1.hw1;
+import java.util.Scanner;
 
 public class Hw1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] splitInput = scanner.nextLine().split(" ");
+        String[] rev = new String[splitInput.length];
+        for (int i=0; i < splitInput.length; i++){
+            rev[i] = splitInput[splitInput.length - i -1].strip();
+        }
+        System.out.println(String.join(" ", rev));
+    }
 }
---
+++ src/main/java/org/example/seminar1/hw1/Hw1_2.java
@@ -0,0 +1,19 @@
+package org.example.seminar1.hw1;
+import java.util.Scanner;
+
+public class Hw1_2 {
+    public static void main(String[] args) {
+        System.out.println(getNum());
+    }
+    private static int getNum(){
+        Scanner scanner = new Scanner(System.in);
+        int acc = 0;
+        int num = 0;
+        while (true) {
+            int new_num = scanner.nextInt();
+            if(new_num > 0) num = new_num;
+            else if(new_num < 0) acc += num;
+            else return acc;
+        }
+    }
+}
